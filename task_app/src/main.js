fddac59ecf759983354a805a4316fcdcf2f155e0
Martyna Michno <mjjmichno@gmail.com>
2021-03-28 21:59:16 +0200

    list component created, some fake data added, installed primevue

--- task_app/src/main.js
+++ task_app/src/main.js
@@ -8,8 +8,14 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "./styles/_classes.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/_bootstrap_changed.scss";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import PrimeVue from "primevue/config";
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(PrimeVue);
 
 Vue.config.productionTip = false;
 
--- task_app/src/views/List.vue
+++ task_app/src/views/List.vue
@@ -1,8 +1,211 @@
 <template>
-  <div>List</div>
+  <div>
+    <b-modal id="comment-modal" hide-footer hide-header>
+      <div>
+        <div
+            class="alert background-pink d-flex align-items-center justify-content-center flex-column mt-4"
+            role="alert"
+        >
+          Comments are unremovable, you can read them only in preview mode.
+        </div>
+        <div class="row mt-4">
+          <div class="col-md-3 d-flex font-weight-500">Comment task</div>
+          <div class="col-md-9">
+            <b-form-textarea
+                id="textarea"
+                v-model="comment"
+                placeholder="Enter task's comment"
+                rows="3"
+            ></b-form-textarea>
+          </div>
+        </div>
+        <div class="row mt-4 d-flex justify-content-end">
+          <b-button @click="hideModal" variant="outline-danger" class="mr-2"
+          >Cancel</b-button
+          >
+          <b-button @click="addComment" variant="success" class="mr-2"
+          >Add comment</b-button
+          >
+        </div>
+      </div>
+    </b-modal>
+    <Card :title="'List of added tasks'">
+      <div
+          class="alert background-pink d-flex align-items-center justify-content-center flex-column mx-2"
+          role="alert"
+      >
+        You can edit or delete only these tasks, which status is "new".
+      </div>
+      <DataTable
+          :value="tasks"
+          show-gridlines
+          class="p-datatable-striped p-datatable-responsive-demo px-2"
+      >
+        <Column header="Title" field="title">
+          <template #body="slotProps">
+            <span class="p-column-title">Title</span>
+            {{ slotProps.data.title }}
+          </template>
+        </Column>
+        <Column header="Performer" field="performer">
+          <template #body="slotProps">
+            <span class="p-column-title">Performer</span>
+            {{ slotProps.data.performer }}
+          </template>
+        </Column>
+        <Column header="Deadline" field="deadline">
+          <template #body="slotProps">
+            <span class="p-column-title">Deadline</span>
+            {{ slotProps.data.deadline }}
+          </template>
+        </Column>
+        <Column header="Edit" field="edit">
+          <template #body="slotProps">
+            <div class="p-column-title">Edit</div>
+            <router-link
+                v-if="slotProps.data.status === 0"
+                :to="{ name: 'EditTask', params: { id: slotProps.data.id } }"
+                class="cursor-pointer"
+            >
+              <b-icon icon="pencil" variant="success"> </b-icon>
+            </router-link>
+          </template>
+        </Column>
+        <Column header="Preview" field="preview">
+          <template #body="slotProps">
+            <div class="p-column-title">Preview</div>
+            <router-link
+                :to="{ name: 'Preview', params: { id: slotProps.data.id } }"
+                class="cursor-pointer"
+            >
+              <b-icon icon="search" variant="success"> </b-icon>
+            </router-link>
+          </template>
+        </Column>
+        <Column header="Add comment" field="addComment">
+          <template #body="slotProps">
+            <div class="p-column-title">Add comment</div>
+            <span
+                class="cursor-pointer"
+                @click="openModalComment(slotProps.data.id)"
+            >
+              <b-icon icon="plus-square-fill" variant="success"> </b-icon>
+            </span>
+          </template>
+        </Column>
+        <Column header="Delete task" field="deleteTask">
+          <template #body="slotProps">
+            <div class="p-column-title">Delete task</div>
+            <span
+                class="cursor-pointer"
+                v-if="slotProps.data.status === 0"
+                @click="deleteTask(slotProps.data.id)"
+            >
+              <b-icon icon="x-square-fill" variant="danger"> </b-icon>
+            </span>
+          </template>
+        </Column>
+      </DataTable>
+    </Card>
+  </div>
 </template>
 <script>
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Card from "../components/shared/Card";
+import Swal from "sweetalert2";
+import {TASKS} from "@/db";
+
 export default {
   name: "List",
+  components: {
+    DataTable,
+    Column,
+    Card,
+  },
+  data() {
+    return {
+      addCommentTaskId: null,
+      comment: "",
+      tasks: [],
+    };
+  },
+  mounted() {
+    this.getTasks();
+  },
+  methods: {
+    getTasks() {
+      console.log('get tasks');
+      this.tasks = TASKS;
+    },
+    deleteTask(id) {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#333d79ff",
+        cancelButtonColor: "#990011FF",
+        confirmButtonText: "Yes, delete it!",
+      }).then(() => {
+        console.log(id)
+      });
+    },
+    openModalComment(id) {
+      this.addCommentTaskId = id;
+      this.$bvModal.show("comment-modal");
+    },
+    hideModal() {
+      this.$bvModal.hide("comment-modal");
+    },
+    addComment() {
+      if (this.comment.length < 3) {
+        Swal.fire({
+          icon: "warning",
+          title: "Your comment is too short!",
+          text: "It needs to be minimum 3 characters.",
+        });
+        return;
+      }
+      console.log('add comm')
+    }
+  },
 };
 </script>
+<style lang="scss">
+.p-datatable-responsive-demo .p-datatable-tbody > tr > td .p-column-title {
+  display: none;
+}
+
+@media screen and (max-width: 500px) {
+  .p-datatable {
+    &.p-datatable-responsive-demo {
+      .p-datatable-thead > tr > th,
+      .p-datatable-tfoot > tr > td {
+        display: none !important;
+      }
+
+      .p-datatable-tbody > tr > td {
+        text-align: left;
+        display: block;
+        width: 100%;
+        float: left;
+        clear: left;
+        border: 0 none;
+
+        .p-column-title {
+          padding: 0.4rem;
+          min-width: 30%;
+          display: inline-block;
+          margin: -0.4em 1em -0.4em -0.4rem;
+          font-weight: bold;
+        }
+
+        &:last-child {
+          border-bottom: 1px solid var(--surface-d);
+        }
+      }
+    }
+  }
+}
+</style>
