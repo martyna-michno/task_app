e6b634ba755c1d4d9d2c6f8a1ea4e5bce94f59ec
Martyna Michno <mjjmichno@gmail.com>
2021-03-29 13:23:20 +0200

    loader added, small fixes

---
+++ task_app/src/views/PageNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <Card>
+<div class="d-flex align-items-center justify-content-center w-100 not-found">
+  page not found
+</div>
+  </Card>
+</template>
+
+<script>
+import Card from "../components/shared/Card";
+export default {
+  name: "Home",
+  components: {
+    Card,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.not-found {
+  font-size: 20px;
+  color: $danger;
+  text-transform: uppercase;
+}
+</style>
